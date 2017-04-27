eb75435489fa607418fc7a2f60539fa213b7038b
Amanda Chappell <Amanda.Chappell@wnco.com>
2017-04-27 16:01:16 -0500

    add a points command

--- lita-jedipoints.rb
+++ lita-jedipoints.rb
@@ -3,27 +3,34 @@ module Lita
     class Jedipoints < Handler
       # insert handler code here
 
-      #require 'firebase'
-      #base_uri = 'https://midi-chlorian-meter.firebaseio.com/'
-      #firebase = Firebase::Client.new(base_uri)
-      #response = firebase.get("users")
-      #response.body.each do |key, array|
-      #    points = firebase.get("events", "orderBy=\"user\"&equalTo=\"#{key}\"")
-      #    score = 0
-      #    points.body.each do |key, array|
-      #      score += array["value"]
-      #    end
-      #    puts "#{key} = #{score}"
-      #end
+      require 'firebase'
+
+      def points(response)
+        base_uri = 'https://midi-chlorian-meter.firebaseio.com/'
+        firebase = Firebase::Client.new(base_uri)
+        firebaseResponse = firebase.get("users")
+        firebaseResponse.body.each do |key, array|
+            points = firebase.get("events", "orderBy=\"user\"&equalTo=\"#{key}\"")
+            score = 0
+            points.body.each do |key, array|
+              score += array["value"]
+            end
+            response.reply("#{key} = #{score}")
+        end
+      end
 
       route(/^echo\s+(.+)/, :echo, command: true, help: {
         "echo TEXT" => "Replies back with TEXT."
       })
 
+      route(/^points\s+(.+)/, :points, command: true, help: {
+        "points" => "Does some points stuff, I hope"
+      }
+
       def echo(response)
         response.reply(response.matches)
       end
-      
+
       Lita.register_handler(self)
     end
   end
