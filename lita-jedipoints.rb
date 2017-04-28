f5bfb98190df4c4c7800fb30176a6ab5373e8c04
Chris K <chris.koeberle@bottlerocketstudios.com>
2017-04-28 14:16:26 -0500

    Actually outpet

--- lita-jedipoints.rb
+++ lita-jedipoints.rb
@@ -114,7 +114,7 @@ module Lita
         firebaseResponse = firebase.get("tasks")
         puts(firebaseResponse.body)
         firebaseResponse.body.each do |entry|
-          puts(entry)
+          response.reply(entry)
         end
      end
 
