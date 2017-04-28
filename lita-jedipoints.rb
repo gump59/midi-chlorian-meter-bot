c8c75a58bfb9958b779ae67c62b67a6659fc6467
Amanda Chappell <amanda.chappell@bottlerocketstudios.com>
2017-04-28 10:27:00 -0500

    one day i’ll learn to spell

--- lita-jedipoints.rb
+++ lita-jedipoints.rb
@@ -38,7 +38,7 @@ module Lita
             score = 0
             points.body.each do |key, array|
               eventDate = Date.parse(array["date"]) rescue nil
-              if eventDate >= month && eventday < month.next_month
+              if eventDate >= month && eventDate < month.next_month
                 score += array["value"]
               end
             end
